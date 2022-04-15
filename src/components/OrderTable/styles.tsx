8224e748af3759f61e6b2a84baab2809ecb13c6e
Aurora <olafrobin@yandex.com>
2022-04-15 04:54:47 -0700

    Reeco Test

--- src/components/OrderTable/styles.tsx
+++ src/components/OrderTable/styles.tsx
@@ -19,21 +19,42 @@ export const Container = styled(BasicContainer)`
   color: black;
   background-color: #f8f4f4;
   overflow: auto;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #ffffffff;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 `;
 
 export const ApproveCheckbox = styled(BasicCheckbox)`
   padding: 10px 20px 10px 20px;
-  background-color: #dbdb37;
+  background-color: #53ed87;
+  color: #ffffff;
+  border: none;
 `;
 
 export const MissingCheckbox = styled(BasicCheckbox)`
   padding: 10px 20px 10px 20px;
   background-color: #b83535;
+  color: #ffffff;
+  border: none;
+  background-color: #ff6941;
 `;
 
 export const MissingUrgentCheckbox = styled(BasicCheckbox)`
   padding: 10px 20px 10px 20px;
   background-color: red;
+  color: #ffffff;
+  border: none;
+  background-color: #fd3135;
 `;
 
 export const MissingModalContainer = styled(ModalContainer)`
